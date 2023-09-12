e472001beb227d8e82c1d0a321fc502cb058a71a
Mathis-Lague <94631012+Mathis-Lague@users.noreply.github.com>
2023-09-12 16:35:44 +0200

    Réalisation d'un premier bouton dynamique

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.css'],
 })
-export class FaceSnapComponent implements OnInit{
+export class FaceSnapComponent implements OnInit {
   title!: string;
   description!: string;
   createdDate!: Date;
   snaps!: number;
+  imageUrl!: string;
+  buttonText!: string;
 
-  ngOnInit(){
+  ngOnInit() {
+
     this.title = 'mathis';
     this.description = 'moi meme';
     this.createdDate = new Date();
     this.snaps = 5;
+    this.imageUrl =
+      'https://images.assetsdelivery.com/compings_v2/uasumy/uasumy1701/uasumy170100015.jpg';
+    this.buttonText = 'Oh Snap !!';
+  }
+
+  onClickSnap() {
+    if(this.buttonText=='Oh Snap !!'){
+      this.snaps++;
+      this.buttonText='Oops un Snap';
+    }
+    else{
+      this.snaps--;
+      this.buttonText='Oh Snap !!';
+    }
+   
   }
 
 }
